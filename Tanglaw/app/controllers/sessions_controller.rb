6d2735a62f11114514073ae6e092a082825e3e3a
Crisostomo Ibarra <christov9999@gmail.com>
2015-11-22 03:47:59 +0800

    Added login

---
+++ Tanglaw/app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+
+  	user = User.find_by(username: params[:session][:username].downcase)
+    # binding.pry
+    if user# && user.authenticate(params[:session][:password])
+		log_in user
+    	redirect_to user
+      # Log the user in and redirect to the user's show page.
+    else
+      # Create an error message.
+      flash[:danger] = 'Invalid email/password combination' # Not quite right!
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
