9a6f10eb48c1641f7ee32ea849cb8cc4a63e7237
Crisostomo Ibarra <christov9999@gmail.com>
2015-11-21 18:38:30 +0800

    Added Code for File Download for PDF

--- Tanglaw/config/routes.rb
+++ Tanglaw/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   get 'static/index'
+  get "/ANN.pdf" => redirect("/ANN.pdf")
+  root 'static#index'
+  get "static/ANN_pdf"
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
