97369d13d667a1f952d3b12fbf4e19f7a50b31e9
christianeyee <christiane.yee@gmail.com>
2015-11-22 01:21:02 +0800

    Added PDF preview

--- Tanglaw/app/controllers/static_controller.rb
+++ Tanglaw/app/controllers/static_controller.rb
@@ -2,12 +2,4 @@ class StaticController < ApplicationController
   def index
   end
   
-  def ANN_pdf
-  send_file(
-    "#{Rails.root}/public/ANN.pdf",
-    filename: "ANN.pdf",
-    disposition: 'inline',
-    type: "application/pdf"
-  )
-  end
 end
